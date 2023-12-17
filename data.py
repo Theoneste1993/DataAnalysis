0798415a039583ec547abda8882b27563a34600a
Theoneste NDAYISENGA <ndayisengatheoneste79@gmail.com>
2023-12-17 11:57:18 +0200

    Hello friends

--- data.py
+++ data.py
@@ -3,11 +3,14 @@ import pandas as pd
 import numpy as np 
 import statistics
 
-x = [12, 34, 545, 75, 75]
-mean_value = statistics.mean(x)
-print("Mean:", mean_value)
+#x = [12, 34, 545, 75, 75]
+#mean_value = statistics.mean(x)
+#print("Mean:", mean_value)
 ### How to learn  new thing 
 import statistics 
 import numpy as pd
 import pandas  as np
+x=[3,3,4,5,24,354,643,63]
+mean_value=statistics.mean(x)
+print("Mean: ",mean_value)
 
